45059268040443769e4eda8d030301a8c22bfaf4
LqZww <951598077@qq.com>
2020-10-29 12:03:24 +0800

    添加icon、icon-position属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,25 @@
 <template>
-  <button class="z-button">按钮</button>
+  <button
+    class="z-button"
+    :class="{[`icon-${iconPosition}`]:true}"
+  >
+    <svg
+      v-if="icon"
+      class="icon"
+    >
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "button",
+  props: ["icon", "iconPosition"],
+};
 </script>
 
 <style lang="scss">
@@ -15,6 +31,12 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  line-height: 1em;
+
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -25,6 +47,25 @@ export default {};
 
   &:focus {
     outline: none;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.4em;
+  }
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.4em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
