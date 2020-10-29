9bf3bf2ee4cadc6eef442e17120d28f4f638e1d6
LqZww <951598077@qq.com>
2020-10-30 00:46:26 +0800

    修复bug：第一个子元素不需要margin-left:-1px;

--- src/button-group.vue
+++ src/button-group.vue
@@ -26,7 +26,10 @@ export default {
 
   > .z-button {
     border-radius: 0;
-    margin-left: -1px;
+
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
 
     &:first-child {
       border-top-left-radius: var(--border-radius);
