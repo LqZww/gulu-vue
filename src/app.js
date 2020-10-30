2bc2ad43165bba22de4dd314bac0c8740fafe01d
LqZww <951598077@qq.com>
2020-10-30 12:04:09 +0800

    添加4个测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,83 @@ new Vue({
     loading3: false
   }
 })
+
+// 单元测试
+import { expect } from 'chai'
+
+// 1. 测试 icon
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  // console.log(vm);
+  vm.$mount()
+
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  // 清理 button 内存
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+// 2. 测试 loading
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+// 3. 测试 iconPosition order 为 1
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+// 4. 测试 iconPosition order 为 2
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
