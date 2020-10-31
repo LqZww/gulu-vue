710842562296532e48ee5d680eb5225a87a2a39c
LqZww <951598077@qq.com>
2020-10-31 22:35:23 +0800

    将app.js测试代码移至test并注释

--- test/button.test.js
+++ test/button.test.js
@@ -77,3 +77,107 @@ describe('Button', () => {
 
   })
 })
+
+// // 单元测试
+// // import { expect } from 'chai';
+// import chai from 'chai';
+// const expect = chai.expect
+
+// import spies from 'chai-spies';
+// chai.use(spies)
+
+
+// // 1. 测试 icon
+// {
+//   const Constructor = Vue.extend(Button)
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: 'settings'
+//     }
+//   })
+//   // console.log(vm);
+//   vm.$mount()
+
+//   let useElement = vm.$el.querySelector('use')
+//   let href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-settings')
+//   // 清理 button 内存
+//   vm.$el.remove()
+//   vm.$destroy()
+// }
+
+// // 2. 测试 loading
+// {
+//   const Constructor = Vue.extend(Button)
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: 'settings',
+//       loading: true
+//     }
+//   })
+//   vm.$mount()
+
+//   let useElement = vm.$el.querySelector('use')
+//   let href = useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-loading')
+//   vm.$el.remove()
+//   vm.$destroy()
+// }
+
+// // 3. 测试 iconPosition order 为 1
+// {
+//   const div = document.createElement('div')
+//   document.body.appendChild(div)
+
+//   const Constructor = Vue.extend(Button)
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: 'settings'
+//     }
+//   })
+//   vm.$mount(div)
+
+//   let svg = vm.$el.querySelector('svg')
+//   let { order } = window.getComputedStyle(svg)
+//   expect(order).to.eq('1')
+//   vm.$el.remove()
+//   vm.$destroy()
+// }
+// // 4. 测试 iconPosition order 为 2
+// {
+//   const div = document.createElement('div')
+//   document.body.appendChild(div)
+
+//   const Constructor = Vue.extend(Button)
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: 'settings',
+//       iconPosition: 'right'
+//     }
+//   })
+//   vm.$mount(div)
+
+//   let svg = vm.$el.querySelector('svg')
+//   let { order } = window.getComputedStyle(svg)
+//   expect(order).to.eq('2')
+//   vm.$el.remove()
+//   vm.$destroy()
+// }
+// // 5. 测试 click
+// {
+//   const Constructor = Vue.extend(Button)
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: 'settings'
+//     }
+//   })
+//   vm.$mount()
+//   let spy = chai.spy(() => {
+
+//   })
+//   vm.$on('click', spy)
+
+//   let button = vm.$el
+//   button.click()
+//   expect(spy).to.have.been.called()
+// }
