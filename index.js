96315d4da828b48f24f62d72629a8169fd77bbbb
LqZww <951598077@qq.com>
2020-11-03 00:13:44 +0800

    响应式及测试用例

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import Button from './src/button.vue'
 import ButtonGroup from './src/button-group.vue'
 import Icon from './src/icon.vue'
 import Input from './src/input.vue'
+import Row from './src/row.vue'
+import Col from './src/col.vue'
 
-export { Button, ButtonGroup, Icon, Input }
+export { Button, ButtonGroup, Icon, Input, Row, Col }
