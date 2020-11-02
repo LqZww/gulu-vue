ab09ca138f648986784944a5a1c9929871449c2d
LqZww <951598077@qq.com>
2020-11-02 18:33:09 +0800

    实现row、col组件

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input.vue'
+import Row from './row.vue'
+import Col from './col.vue'
 
 
 Vue.component('z-button', Button)
 Vue.component('z-icon', Icon)
 Vue.component('z-button-group', ButtonGroup)
 Vue.component('z-input', Input)
+Vue.component('z-row', Row)
+Vue.component('z-col', Col)
 
 
 new Vue({
---
+++ src/row.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="row"
+    :style="{marginLeft : -gutter / 2 + 'px', marginRight : -gutter / 2 + 'px'}"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "z-row",
+  data() {
+    return {};
+  },
+  props: {
+    gutter: {
+      type: [Number, String],
+    },
+  },
+  mounted() {
+    // console.log(this.$children);
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.row {
+  display: flex;
+}
+</style>
